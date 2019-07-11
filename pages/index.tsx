82df56a5e79c6f45f3479afda0a19fd4bd24ab86
gureuso <wyun13043@gmail.com>
2019-07-11 21:15:17 +0900

    Fix title style

--- pages/index.tsx
+++ pages/index.tsx
@@ -108,8 +108,8 @@ class Title extends React.Component {
   render() {
     return (
       <div className="content bg-c-gray" id="title">
+        <p>걱정없이 집을 나가자!</p>
         <h1>EASYWAY</h1>
-        <p>걱정없이 집을 나가자! EASYWAY!</p>
       </div>
     );
   }
