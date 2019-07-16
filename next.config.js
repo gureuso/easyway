6fed28431a538c2ab655cbee25851b8787b8d38a
gureuso <wyun13043@gmail.com>
2019-07-17 00:55:36 +0900

    Add less css preprocessor

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-const withCSS = require('@zeit/next-css')
+const withCSS = require('@zeit/next-css');
+const withLess = require('@zeit/next-less');
 
-module.exports = withCSS({
-  webpack(config) {
-    config.node = {
-      fs: 'empty'
-    }
-    return config
-  }
-})
+module.exports = withCSS(withLess({
+  
+}));
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import App, { Container } from 'next/app';
 
 import 'normalize.css';
-import '../static/css/style.css';
+import '../static/css/style.less';
 
 class MyApp extends App {
   static async getInitialProps({ Component, ctx }: { Component: any, ctx: any }) {
