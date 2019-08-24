a0863e6b42165b97aa582e5f11d094a808af4f1f
gureuso <wyun13043@gmail.com>
2019-08-24 15:16:25 +0900

    Add cc for subway info

--- pages/cc.tsx
+++ pages/cc.tsx
@@ -5,6 +5,7 @@ class CCPage extends React.Component {
     return (
       <ul>
         <li><a href="https://www.data.go.kr/dataset/15000314/openapi.do">버스도착정보</a> by 서울특별시, CC BY</li>
+        <li><a href="http://data.seoul.go.kr/dataList/datasetView.do?infId=OA-12764&srvType=A&serviceKind=1&currentPageNo=1">지하철도착정보</a> by 서울특별시, CC BY</li>
       </ul>
     );
   }
