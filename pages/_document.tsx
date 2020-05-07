fdf053bf2646b07a3c1490ed9131b9b6db0b34c5
gureuso <wyun13043@gmail.com>
2020-05-07 14:19:11 +0900

    Clean code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,6 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-          <meta charSet="utf-8"/>
         </Head>
         <body>
           <Main/>
